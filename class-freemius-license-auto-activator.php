7ec73245c67c3ee52505fb550d2cbaa1b93f56b0
Peter Wise <peter@squarecandy.net>
2022-07-15 12:27:41 -0400

    remove check for "pending"; add CLI/debug output

--- class-freemius-license-auto-activator.php
+++ class-freemius-license-auto-activator.php
@@ -1,4 +1,5 @@
 <?php
+/* phpcs:disable Generic.ControlStructures.InlineControlStructure.NotAllowed */
 class Freemius_License_Auto_Activator {
 	private $priv_shortcode;
 
@@ -10,6 +11,7 @@ class Freemius_License_Auto_Activator {
 	public function license_key_auto_activation() {
 
 		$fs = false;
+		if ( WP_DEBUG || FREEMIUS_WP_CLI ) echo "license_key_auto_activation function started\n";
 
 		if ( function_exists( $this->priv_shortcode ) ) {
 			$fs = ( $this->priv_shortcode )();
@@ -19,35 +21,41 @@ class Freemius_License_Auto_Activator {
 		}
 
 		if ( empty( $fs ) ) {
+			if ( WP_DEBUG || FREEMIUS_WP_CLI ) echo __LINE__ . " fs is empty \n";
 			return;
 		}
 
-		if ( ! $fs->has_api_connectivity() || $fs->is_registered() ) {
-			// No connectivity OR the user already opted-in to Freemius.
+		if ( ! $fs->has_api_connectivity() ) {
+			if ( WP_DEBUG || FREEMIUS_WP_CLI ) echo "Error: no API connectivity\n";
+			return;
+		}
+
+		if ( $fs->is_registered() ) {
+			if ( WP_DEBUG || FREEMIUS_WP_CLI ) echo "Error: The user already opted-in to Freemius\n";
 			return;
 		}
 
 		$option_key = "{$this->priv_shortcode}_auto_license_activation";
-
-		if ( 'pending' !== get_option( $option_key, 'pending' ) ) {
-			return;
-		}
+		if ( WP_DEBUG || FREEMIUS_WP_CLI ) echo __LINE__ . " $option_key \n";
 
 		try {
 			$key       = constant( 'WP__' . strtoupper( $this->priv_shortcode ) . '__LICENSE_KEY' );
 			$next_page = $fs->activate_migrated_license( $key );
 		} catch ( Exception $e ) {
+			if ( WP_DEBUG || FREEMIUS_WP_CLI ) echo __LINE__ . " Error: $e->message \n";
 			update_option( $option_key, 'unexpected_error' );
 			return;
 		}
 
 		if ( $fs->can_use_premium_code() ) {
 			update_option( $option_key, 'done' );
+			if ( WP_DEBUG || FREEMIUS_WP_CLI ) echo __LINE__ . "Success: license key install is done.\n";
 
 			if ( is_string( $next_page ) ) {
 				fs_redirect( $next_page );
 			}
 		} else {
+			if ( WP_DEBUG || FREEMIUS_WP_CLI ) echo "Error: license key install failed \n";
 			update_option( $option_key, 'failed' );
 		}
 	}
